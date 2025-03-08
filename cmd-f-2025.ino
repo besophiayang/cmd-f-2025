f48aee3f41beff77244b5c8e204f3058ad2855a0
besophiayang <besophiayang@gmail.com>
2025-03-08 15:22:08 -0800

    Added readDistance function and Wifi setup stuff

--- cmd-f-2025.ino
+++ cmd-f-2025.ino
@@ -5,16 +5,36 @@
 
 const int trigPin = 5;
 const int echoPin = 18;
+#define SOUND_SPEED 0.034
+
+long duration;
+float distanceCm;
+
+const char* ssid = "WiFi"
+const char* password = "WiFi-Password"
+
+String readDistance() {
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+    distanceCm = duration * SOUND_SPEED / 2;
+    Serial.println("Distance:");
+    Serial.println(distanceCm);
+    return String(distanceCm);
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+ 
+    WiFi.begin(ssid, password);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(readDistance());
+  delay(1000);
 }
