7b090d12d0e43ef496964ca91d494b4cf6d52757
Tiffany Yang <betiffanyyang@gmail.com>
2025-03-08 13:58:15 -0800

    Added ultrasonic sensor pin code

--- cmd-f-2025.ino
+++ cmd-f-2025.ino
@@ -1,5 +1,16 @@
+#include <ESPmDNS.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include <Adafruit_Sensor.h>
+
+const int trigPin = 5;
+const int echoPin = 18;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 
 }
 
