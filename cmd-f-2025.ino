6bb95d94473d86ddea85e6bdd762371c6fbb5060
besophiayang <besophiayang@gmail.com>
2025-03-08 17:22:05 -0800

    Finished WiFi code and added basic stepper motor initialization code

--- cmd-f-2025.ino
+++ cmd-f-2025.ino
@@ -6,9 +6,17 @@
 const int trigPin = 5;
 const int echoPin = 18;
 #define SOUND_SPEED 0.034
+#define DISTANCE_THRESHOLD 7.0
 
 long duration;
 float distanceCm;
+
+const int stepsPerRevolution = 2048;
+#define IN1 19
+#define IN2 21
+#define IN3 15
+#define IN4 2
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 
 const char* ssid = "WiFi"
 const char* password = "WiFi-Password"
@@ -29,8 +37,26 @@ void setup() {
   Serial.begin(115200);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
- 
+
+    WiFi.disconnect(true); 
     WiFi.begin(ssid, password);
+    Serial.println("Connecting to Wi-Fi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("Connected to Wi-Fi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    if (!MDNS.begin("esp32")) {
+        Serial.println("Error starting mDNS");
+    } else {
+        Serial.println("mDNS started");
+    }
+
+    server.begin();
+    Serial.println("HTTP server started");
 
 }
 
